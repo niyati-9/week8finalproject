e66d41015f933bc0b0d20dacde71b49cb356c69b
Niyati Malla <mallaniyati19@gmail.com>
2024-10-20 19:21:55 +0545

    Add error handling and result display to BMI calculator

--- bmi_calculator/bmiapp/forms.py
+++ bmi_calculator/bmiapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 EXERCISE_CHOICES = [
     ('sedentary', 'Sedentary (little to no exercise)'),
@@ -15,3 +16,35 @@ class BMIForm(forms.Form):
     estimated_weeks = forms.IntegerField(label='Estimated Weeks')
     age = forms.IntegerField(label='Age')
     exercise_level = forms.ChoiceField(choices=EXERCISE_CHOICES, label='Exercise Level')
+
+    def clean_current_weight(self):
+        weight = self.cleaned_data['current_weight']
+        if weight <= 0:
+            raise ValidationError("Weight must be greater than 0.")
+        return weight
+
+    def clean_height(self):
+        height = self.cleaned_data['height']
+        if height <= 0:
+            raise ValidationError("Height must be greater than 0.")
+        return height
+
+    def clean_goal_weight(self):
+        weight = self.cleaned_data['goal_weight']
+        if weight <= 0:
+            raise ValidationError("Goal weight must be greater than 0.")
+        return weight
+
+    def clean_estimated_weeks(self):
+        weeks = self.cleaned_data['estimated_weeks']
+        if weeks <= 0:
+            raise ValidationError("Estimated weeks must be greater than 0.")
+        return weeks
+
+    def clean_age(self):
+        age = self.cleaned_data['age']
+        if age <= 0:
+            raise ValidationError("Age must be greater than 0.")
+        if age > 120:
+            raise ValidationError("Please enter a valid age.")
+        return age
